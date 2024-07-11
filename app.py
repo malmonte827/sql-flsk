f621d97d195fb712247e3c26e457792f75801c32
Michael <malmonte454@proton.me>
2024-07-11 16:23:39 -0400

    routing complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from models import db, connect_db, Pet
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -18,9 +18,18 @@ def list_pets():
     pets = Pet.query.all()
     return render_template('list.html', pets=pets)
 
-@app.route( "/" )
+@app.route( "/", methods=['POST'] )
 def creat_pet():
-    return 'yousent your data'
+    name = request.form['name']
+    species = request.form['species']
+    hunger = request.form['hunger']
+    hunger = int(hunger) if hunger else None
+
+    new_pet = Pet(name=name, species=species, hunger=hunger)
+    db.session.add(new_pet)
+    db.session.commit()
+
+    return redirect(f'/{new_pet.id}')
 
 @app.route( '/<int:pet_id>' )
 def show_pet(pet_id):
@@ -29,3 +38,7 @@ def show_pet(pet_id):
     return render_template('details.html', pet=pet)
 
 
+@app.route('/species/<species_id>')
+def show_pets_by_species(species_id):
+    pets = Pet.get_by_species(species_id)
+    return render_template('species.html', pets=pets, species=species_id)
